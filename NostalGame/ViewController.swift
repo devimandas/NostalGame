80a1aafdd07038227257b076a4ef17aa4a4432b9
No Brainers <www.infinitelearning.id>
2020-03-30 23:36:22 +0700

    Initial Commit

---
+++ NostalGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NostalGame
+//
+//  Created by Devi Mandasari on 30/03/20.
+//  Copyright © 2020 Devi Mandasari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NostalGameTests/NostalGameTests.swift
@@ -0,0 +1,34 @@
+//
+//  NostalGameTests.swift
+//  NostalGameTests
+//
+//  Created by Devi Mandasari on 30/03/20.
+//  Copyright © 2020 Devi Mandasari. All rights reserved.
+//
+
+import XCTest
+@testable import NostalGame
+
+class NostalGameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
